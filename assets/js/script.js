d4390bdb584b2640bc287e695f1d9082297b8a7b
jaredtrp <jaredtrp@gmail.com>
2021-12-18 18:29:35 -0600

    got search field and button requesting from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,40 @@
 const containerLeftEl = document.querySelector('#container-left');
 const containerRightEl = document.querySelector('#container-right');
 const input = document.querySelector('#search-form');
-const searchForm = document.querySelector('#form-container');
 const searchButtonEl = document.querySelector('#search-button');
 const cityHeroEl = document.querySelector('#city-hero');
 const forecastEl = document.querySelector('#forecast');
 
-// let searchedCity = ;
-let lat = 29.7633;
-let lon = -95.3633;
-let city = 'Houston';
-let state = 'US';
+let city = '';
 
-button.addEventListener('click', function(){
-    let requestHeroURL = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&exclude=hourly&units=imperial&appid=73963c93b7e24695087cf9963cd9fc41`
-
-    fetch(requestHeroURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        var cityValue = data['location']['name'];
-        var stateValue = data['location']['region'];
-        var tempValue = data['current']['temp_f'];
-        var condValue = data['current']['condition']['text'];
-    });
-
-    let requestForecastURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=73963c93b7e24695087cf9963cd9fc41`
-
-    fetch(requestForecastURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        var cityValue = data['location']['name'];
-        var stateValue = data['location']['region'];
-        var tempValue = data['current']['temp_f'];
-        var condValue = data['current']['condition']['text'];
-    });
-})
+searchButtonEl.addEventListener('click', function searchedCity(event) {
+	event.preventDefault();
+	city = document.querySelector("input[name='search']").value;
+    console.log(city);
+	// check if inputs are empty (validate)
+	if (!city) {
+		alert("You must enter a city!");
+		return false;
+	}
+    getRequestedWeather();
+	// reset form field for next city to be entered
+	document.querySelector("input[name='search']").value = "";
 });
+
+let getRequestedWeather = function (){
+
+    let requestWeather = `https://api.openweathermap.org/data/2.5/forecast?q=${city},US&units=imperial&appid=73963c93b7e24695087cf9963cd9fc41`
+
+    fetch(requestWeather)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        // var cityValue = data['location']['name'];
+        // var stateValue = data['location']['region'];
+        // var tempValue = data['current']['temp_f'];
+        // var condValue = data['current']['condition']['text'];
+    });
+};
+
