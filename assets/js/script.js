bd0c817f7b6e42f56d4e601b3e0ca328004308b1
jaredtrp <jaredtrp@gmail.com>
2021-12-19 19:02:31 -0600

    Pulled data and stored in variables for main weather card

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ const searchButtonEl = document.querySelector('#search-button');
 const cityHeroEl = document.querySelector('#city-hero');
 const forecastEl = document.querySelector('#forecast');
 
-let city = '';
 let cityValue = [];
 let currentWeather = '';
 
+// Function to gather input from search
 searchButtonEl.addEventListener('click', function searchedCity(event) {
 	event.preventDefault();
 	city = document.querySelector("input[name='search']").value;
@@ -23,6 +23,7 @@ searchButtonEl.addEventListener('click', function searchedCity(event) {
 	document.querySelector("input[name='search']").value = "";
 });
 
+// Function to pull data from API
 let getRequestedWeather = function (){
 
     let requestWeather = `https://api.openweathermap.org/data/2.5/forecast?q=${city},US&units=imperial&appid=73963c93b7e24695087cf9963cd9fc41`
@@ -33,10 +34,23 @@ let getRequestedWeather = function (){
     })
     .then(function(data) {
         console.log(data);
-        var cityValue = data.list[0];
+
+        let cityData = data.city.name;
+        cityValue.push(cityData);
+        let tempValue = data.list[0].main.temp;
+        let humiValue = data.list[0].main.humidity;
+        let windValue = data.list[0].wind.speed;
         console.log(cityValue);
-        // var tempValue = data['current']['temp_f'];
-        // var condValue = data['current']['condition']['text'];
+        console.log(tempValue);
+        console.log(humiValue);
+        console.log(windValue);
+    displayWeather();
+        function displayWeather() {
+            let cityNameText = document.createElement('h1');
+            cityNameText.id = 'cityNameText' + workHours[i];
+            cityNameText.classList = 'cityNameText col-1';
+            containerSections.appendChild(cityNameText);
+        }
     });
 };
 
